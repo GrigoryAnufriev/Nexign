5f885272d7a7ece2ce948c6dd2c99a311a455c4e
GrigoryAnufriev <anufriech02@gmail.com>
2023-03-27 03:52:37 +1000

    Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,14 +9,6 @@ public class Main {
     public static void main(String[] args) {
 
         GenerateReport.GenerateReports();
-//        List<String> s = new ArrayList<>(Parser.parse().getCDRTable().keySet());
-//        for (String st : s)
-//            System.out.println(st);
-//        CDR cdr = Parser.parse("02, 79876543221, 20230321160455, 20230321163211, 11");
-//        System.out.println(cdr.getDateTimeOfBegin());
-//        System.out.println(cdr.getDateTimeOfEnd());
-//
-//        System.out.println(ChronoUnit.SECONDS.between(cdr.getDateTimeOfBegin(), cdr.getDateTimeOfEnd()));
-//        System.out.println(cdr.getNumber());
+
     }
 }
