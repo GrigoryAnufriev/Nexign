b685fb5d641cf8bfb610138cf5e991210e109e39
GrigoryAnufriev <anufriech02@gmail.com>
2023-03-27 03:54:09 +1000

    Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,3 @@
-import models.CDR;
-
-import java.time.temporal.ChronoUnit;
-import java.util.ArrayList;
-import java.util.List;
-
 public class Main {
 
     public static void main(String[] args) {
